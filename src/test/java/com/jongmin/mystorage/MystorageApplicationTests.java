8c3f1d28fc2b36cf9d2d369247292deb9694bc5a
jongminzzang97 <jongminzzang97@gmail.com>
2024-01-13 16:21:35 +0900

    chore: setting project

---
+++ src/test/java/com/jongmin/mystorage/MystorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongmin.mystorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
