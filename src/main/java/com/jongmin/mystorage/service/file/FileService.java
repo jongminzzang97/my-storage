613b235d0463ce2b4a031e4708c85d4d3b1c0531
jongminzzang97 <jongminzzang97@gmail.com>
2024-02-26 17:07:49 +0900

    refactor: FileSystemWrapper의 내용을 FileIoUtils로 이동

--- src/main/java/com/jongmin/mystorage/service/file/FileService.java
+++ src/main/java/com/jongmin/mystorage/service/file/FileService.java
@@ -33,13 +33,52 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class FileService {
 
-	private final FileSystemWrapper fileSystemWrapper;
 	private final FileRepository fileRepository;
 
 	private final FolderRepositoryUtils folderRepositoryUtils;
 	private final FileRepositoryUtils fileRepositoryUtils;
 
 	private final FileIoUtils fileIoUtils;
+
+	private MyFile checkMyFileAndGet(String ownerName, UUID fileUuid) {
+		Optional<MyFile> optional = fileRepository.findByUuid(fileUuid);
+		if (optional.isEmpty()) {
+			throw new FileNotInDatabaseException("파일에 대한 정보가 DB에 존재하지 않습니다.");
+		}
+
+		MyFile myFile = optional.get();
+		if (!myFile.getOwnerName().equals(ownerName)) {
+			throw new RuntimeException("본인 소유의 파일이 아닙니다.");
+		}
+
+		if (fileIoUtils.fileNotExists(myFile)) {
+			throw new FileNotInFileSystemException("다운로드 하려는 파일이 존재하지 않습니다.");
+		}
+		return myFile;
+	}
+
+	public Resource downloadFile(String ownerName, UUID fileUuid) {
+		// 파일을 다운로드를 위한 Lock을 얻고,
+		// 파일 상태를 확인하고 다운로드 받는 과정이 atomic하게 진행될 수 있도록 추후에 수정이 필요합니다.
+		MyFile checkedFile = checkMyFileAndGet(ownerName, fileUuid);
+		return fileIoUtils.fileToResource(checkedFile);
+	}
+
+	public FileResponse readFile(String ownerName, UUID fileUuid) {
+		MyFile checkedFile = checkMyFileAndGet(ownerName, fileUuid);
+		return FileResponse.fromMyFile(checkedFile);
+	}
+
+	public StringResponse deleteFile(String ownerName, UUID fileUuid) {
+		// 파일을 삭제를 위한 Lock을 얻고,
+		// 파일 상태를 확인하고 삭제하는 과정이 atomic하게 진행될 수 있도록 추후에 수정이 필요합니다.
+		// 파일을 다운로드 받는 사용자가 있는 경우 역시 고려되어야 합니다.
+		MyFile checkedFile = checkMyFileAndGet(ownerName, fileUuid);
+		fileIoUtils.deleteFile(checkedFile);
+		fileRepositoryUtils.deleteFile(checkedFile);
+
+		return new StringResponse("요청한 파일에 대한 삭제가성공적으로 진행되었습니다.");
+	}
 
 	public FileResponse uploadFile(String ownerName, UploadFileRequestDto requestDto) {
 		String fileName = requestDto.getMultipartFile().getName();
@@ -49,84 +88,12 @@ public class FileService {
 
 		MyFolder parentFolder = folderRepositoryUtils.getFolderByUuid(requestDto.getFolderUuid());
 		MyFile myFileEntity = MyFile.createMyFileEntity(requestDto.getMultipartFile(), ownerName, parentFolder);
-
-		String fileDir = myFileEntity.getAccessRoute();
-		if (fileSystemWrapper.fileExists(fileDir)) {
+		if (fileIoUtils.fileExists(myFileEntity)) {
 			throw new FileAlreadyExistException("이미 동일한 이름의 파일이 존재합니다.");
 		}
 
-		try (InputStream inputStream = requestDto.getMultipartFile().getInputStream()) {
-			long copy = fileSystemWrapper.copy(inputStream, fileDir);
-		} catch (IOException e) {
-			throw new FileStorageException("파일 저장 중 오류가 발생했습니다.", e);
-		}
-
+		fileIoUtils.save(requestDto.getMultipartFile(), myFileEntity);
 		fileRepository.save(myFileEntity);
 		return FileResponse.fromMyFile(myFileEntity);
 	}
-
-	public Resource downloadFile(String ownerName, UUID fileUuid) {
-		// 파일을 다운로드를 위한 Lock을 얻고,
-		// 파일 상태를 확인하고 다운로드 받는 과정이 atomic하게 진행될 수 있도록 추후에 수정이 필요합니다.
-		Optional<MyFile> optional = fileRepository.findByUuid(fileUuid);
-		if (optional.isEmpty()) {
-			throw new FileNotInDatabaseException("파일에 대한 정보가 DB에 존재하지 않습니다.");
-		}
-
-		MyFile myFile = optional.get();
-		if (!myFile.getOwnerName().equals(ownerName)) {
-			throw new RuntimeException("본인 소유의 파일이 아닙니다.");
-		}
-
-		String accessRoute = myFile.getAccessRoute();
-		if (fileSystemWrapper.fileNotExists(accessRoute)) {
-			throw new FileNotInFileSystemException("다운로드 하려는 파일이 존재하지 않습니다.");
-		}
-
-		return fileSystemWrapper.fileDirToResource(accessRoute);
-	}
-
-	public FileResponse readFile(String ownerName, UUID fileUuid) {
-		Optional<MyFile> optional = fileRepository.findByUuid(fileUuid);
-		if (optional.isEmpty()) {
-			throw new FileNotInDatabaseException("파일에 대한 정보가 DB에 존재하지 않습니다.");
-		}
-
-		MyFile myFile = optional.get();
-		if (!myFile.getOwnerName().equals(ownerName)) {
-			throw new RuntimeException("본인 소유의 파일이 아닙니다.");
-		}
-
-		String accessRoute = myFile.getAccessRoute();
-		if (fileSystemWrapper.fileNotExists(accessRoute)) {
-			throw new FileNotInFileSystemException("다운로드 하려는 파일이 존재하지 않습니다.");
-		}
-
-		return FileResponse.fromMyFile(myFile);
-	}
-
-	public StringResponse deleteFile(String ownerName, UUID fileUuid) {
-		// 파일을 삭제를 위한 Lock을 얻고,
-		// 파일 상태를 확인하고 삭제하는 과정이 atomic하게 진행될 수 있도록 추후에 수정이 필요합니다.
-		// 파일을 다운로드 받는 사용자가 있는 경우 역시 고려되어야 합니다.
-		Optional<MyFile> optional = fileRepository.findByUuid(fileUuid);
-		if (optional.isEmpty()) {
-			throw new FileNotInDatabaseException("파일에 대한 정보가 DB에 존재하지 않습니다.");
-		}
-
-		MyFile myFile = optional.get();
-		if (!myFile.getOwnerName().equals(ownerName)) {
-			throw new RuntimeException("본인 소유의 파일이 아닙니다.");
-		}
-		String accessRoute = myFile.getAccessRoute();
-		if (fileSystemWrapper.fileNotExists(accessRoute)) {
-			throw new FileNotInFileSystemException("삭제 하려는 파일이 존재하지 않습니다.");
-		}
-
-		fileIoUtils.deleteFile(myFile);
-		fileRepositoryUtils.deleteFile(myFile);
-
-		return new StringResponse("요청한 파일에 대한 삭제가성공적으로 진행되었습니다.");
-	}
-
 }
--- src/main/java/com/jongmin/mystorage/utils/ioutils/FileIoUtils.java
+++ src/main/java/com/jongmin/mystorage/utils/ioutils/FileIoUtils.java
@@ -1,12 +1,17 @@
 package com.jongmin.mystorage.utils.ioutils;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.jongmin.mystorage.exception.FileStorageException;
 import com.jongmin.mystorage.model.MyFile;
@@ -20,6 +25,24 @@ public class FileIoUtils {
 	@Value("${file.storage.baseDir}")
 	private String baseDir;
 
+	public boolean fileExists(MyFile myFile) {
+		String accessRoute = myFile.getAccessRoute();
+		Path path = Paths.get(baseDir + accessRoute);
+		return Files.exists(path);
+	}
+
+	public boolean fileNotExists(MyFile myFile) {
+		String accessRoute = myFile.getAccessRoute();
+		Path path = Paths.get(baseDir + accessRoute);
+		return Files.notExists(path);
+	}
+
+	public Resource fileToResource(MyFile myFile) {
+		String accessRoute = myFile.getAccessRoute();
+		Path path = Paths.get(baseDir + accessRoute);
+		return new FileSystemResource(path);
+	}
+
 	public void deleteFile(MyFile myFile) {
 		String accessRoute = myFile.getAccessRoute();
 		Path path = Paths.get(baseDir + accessRoute);
@@ -27,6 +50,16 @@ public class FileIoUtils {
 			Files.delete(path);
 		} catch (IOException e) {
 			throw new FileStorageException("파일 삭제 중 오류가 발생했습니다.");
+		}
+	}
+
+	public long save(MultipartFile multipartFile, MyFile myFile, CopyOption... options) {
+		String accessRoute = myFile.getAccessRoute();
+		Path path = Paths.get(baseDir + accessRoute);
+		try (InputStream inputStream = multipartFile.getInputStream()) {
+			return Files.copy(inputStream, path, options);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
