0afce305d40db77b7abe3406580f8317e11aa745
jongminzzang97 <jongminzzang97@gmail.com>
2024-03-08 23:40:38 +0900

    feat: StorageInfo 모델 추가

---
+++ src/main/java/com/jongmin/mystorage/model/StorageInfo.java
@@ -0,0 +1,40 @@
+package com.jongmin.mystorage.model;
+
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class StorageInfo extends BaseEntity {
+	private String ownerName;
+	private Long size;
+	private Long fileCount;
+	private Long folderCount;
+
+	@Builder
+	public StorageInfo(String ownerName, Long size, Long fileCount, Long folderCount) {
+		this.ownerName = ownerName;
+		this.size = size;
+		this.fileCount = fileCount;
+		this.folderCount = folderCount;
+	}
+
+	public StorageInfo(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	public void setSize(Long size) {
+		this.size = size;
+	}
+
+	public void setFileCount(Long fileCount) {
+		this.fileCount = fileCount;
+	}
+
+	public void setFolderCount(Long folderCount) {
+		this.folderCount = folderCount;
+	}
+}
